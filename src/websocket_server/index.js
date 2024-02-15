1d12042242efab8c5225ac2b9c86fa2fc76eb979
pavel_hladki <pavel_hladki@senla.eu>
2024-02-15 23:16:30 +0300

    init: ws server

---
+++ src/websocket_server/index.js
@@ -0,0 +1,54 @@
+import { WebSocketServer, createWebSocketStream } from 'ws';
+
+export const initWebSocketServer = () => {
+  const SERVER_PORT = 3000;
+
+  const sockets = [];
+  
+  const webSocketServer = new WebSocketServer({ port: SERVER_PORT}, () => console.log(`Web Socket server on the ${SERVER_PORT} port!`) );
+  
+  webSocketServer.on('connection', (ws, req) => {
+    sockets.push(ws);
+    const wsStream = createWebSocketStream(ws, { encoding: 'utf8', decodeStrings: false });
+  
+    console.log((`WS_params:${JSON.stringify(req.socket.address())}`))
+  
+    wsStream.on('data', async (rawData) => {
+      const { type, data, id } = JSON.parse(rawData);
+      console.log(rawData);
+
+      switch (type) {
+        case 'reg': {
+          const resData = JSON.stringify({
+            type,
+            id,
+            data: JSON.stringify({
+              name: data.name,
+              index: 0,
+              error: false,
+              errorText: "",
+            })
+          })
+
+          ws.send(resData);
+        }
+        case 'create_room': {
+          const resData = JSON.stringify({
+            type,
+            id,
+            data: JSON.stringify({
+              idGame: 0,
+              idPlayer: 0,
+            })
+          })
+
+          ws.send(resData);
+        }
+        case 'update_room': {
+          console.log('update_room');
+        }
+      }
+    });
+    
+  });
+}
